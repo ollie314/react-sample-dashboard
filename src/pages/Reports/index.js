81889bb3688dc826012ca69f20ebdf1323ad4b91
ollie314 <mehdi.lefebvre@gmail.com>
2022-02-10 13:47:49 +0000

    feat: add base idea for reporting
    Signed-off-by: ollie314 <mehdi.lefebvre@gmail.com>

--- src/pages/Reports/index.js
+++ src/pages/Reports/index.js
@@ -8,21 +8,76 @@ import Paper from '@mui/material/Paper';
 
 import Title from '../../components/Title';
 
+import ExchangesRepartition from './ExchangesRepartition';
+import RateInfoChart from './RateInfoChart';
+
 const Reports = () => {
   return (
     <>
-      <Grid container spacing={3}>
-        {/* Chart */}
+      <Grid container spacing={1}>
+        <Grid item xs={4} md={4} lg={4}>
+          <Paper
+            sx={{
+              p: 2,
+              display: 'flex',
+              flexDirection: 'column',
+              height: 400,
+            }}
+          >
+            <Title>Reception Rate</Title>
+            <RateInfoChart />
+          </Paper>
+        </Grid>
+        <Grid item xs={4} md={4} lg={4}>
+          <Paper
+            sx={{
+              p: 2,
+              display: 'flex',
+              flexDirection: 'column',
+              height: 400,
+            }}
+          >
+            <Title>Emission Rate</Title>
+            <RateInfoChart />
+          </Paper>
+        </Grid>
+        <Grid item xs={4} md={4} lg={4}>
+          <Paper
+            sx={{
+              p: 2,
+              display: 'flex',
+              flexDirection: 'column',
+              height: 400,
+            }}
+          >
+            <Title>Error Rate</Title>
+            <RateInfoChart />
+          </Paper>
+        </Grid>
         <Grid item xs={12} md={12} lg={12}>
           <Paper
             sx={{
               p: 2,
               display: 'flex',
               flexDirection: 'column',
-              height: 240,
+              height: 400,
             }}
           >
-            <Title>Reports</Title>
+            <Title>Transfert type repartition for emission</Title>
+            <ExchangesRepartition />
+          </Paper>
+        </Grid>
+        <Grid item xs={12} md={12} lg={12}>
+          <Paper
+            sx={{
+              p: 2,
+              display: 'flex',
+              flexDirection: 'column',
+              height: 400,
+            }}
+          >
+            <Title>Transfert type repartition for reception</Title>
+            <ExchangesRepartition />
           </Paper>
         </Grid>
       </Grid>
